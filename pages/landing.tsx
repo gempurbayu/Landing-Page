c3321e82f86e2861a8c8cdb3fb7d825c324a0fd6
gempurbayu <aji123zz@gmail.com>
2022-07-03 17:12:59 +0700

    consuming weather api

---
+++ pages/landing.tsx
@@ -0,0 +1,9 @@
+export default function landing() {
+  return (
+    <div className="bg-[url('../assets/img/bg-header.png')] w-screen h-screen bg-cover">
+        <div className="container m-auto">
+            <h1>Hello There, I'm Gempur Bayu Aji</h1>
+        </div>
+    </div>
+  )
+}
